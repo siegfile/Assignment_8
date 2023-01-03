9f18d3c58f299be20b0f777f68443da7c2ebed01
siegfile <dkruchek@kse.org.ua>
2023-01-03 11:27:34 +0200

    basic paddle structure

--- main.py
+++ main.py
@@ -1,15 +1,29 @@
+
 import pgzrun
+from pgzero.actor import Actor
 
 WIDTH = 600
 HEIGHT = 800
 
+class Paddle:
 
+    def __init__(self):
+        self.actor = Actor('paddle.png', center = (WIDTH//2, HEIGHT-25))
+
+    def draw(self):
+        self.actor.draw()
+
+paddle = Paddle()
 def draw():
-    pass
+    screen.clear()
+    paddle.draw()
 
 
 def update(dt):
     pass
+
+def on_mouse_move(pos):
+    paddle.actor.x = pos[0]
 
 
 pgzrun.go()
